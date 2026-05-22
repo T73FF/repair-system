da39df8445a789212801c2923310dee045523c97
TREF20 <mielnikov953@gmail.com>
2026-05-23 01:08:02 +0500

    Fix storage permissions via start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Фикс прав на storage и bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Ждём, пока база данных поднимется (опционально)
+sleep 5
+
+# Запускаем миграции
 php artisan migrate --force
 
+# Запускаем PHP-FPM в фоне
 php-fpm -D
 
+# Запускаем Nginx
 nginx -g "daemon off;"
