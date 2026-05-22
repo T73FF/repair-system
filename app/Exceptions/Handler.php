cc83589aab4ea625bfec674029db1b288a0bb0f6
TREF20 <mielnikov953@gmail.com>
2026-05-23 00:14:01 +0500

    Fix push notifications: direct WebPushChannel without queue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,15 +4,19 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Illuminate\Database\QueryException;
 
-public function register(): void
+class Handler extends ExceptionHandler
 {
-    $this->reportable(function (Throwable $e) {
-        if (str_contains($e->getMessage(), 'GMP or BCMath extension')) {
-            return false;
-        }
-    });
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
 }
-
-
